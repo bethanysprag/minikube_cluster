499aa4d02c0dcaa3439e2a39e5cd89f4259f7b19
bethany sprague <bsprague@gmail.com>
2023-10-02 23:08:06 +0000

    blah

---
+++ scripts/install_kompose.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Linux
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.31.0/kompose-linux-amd64 -o kompose
+chmod +x kompose
+sudo mv ./kompose /usr/local/bin/kompose
